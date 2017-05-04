55a7fc1dd58845e283c9870d887c63ebe782a8c6
Levin Keller <post@levinkeller.de>
2017-05-04 18:37:36 +0200

    Slight improvements.

--- commands.sh
+++ commands.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 docker-cloud stack up -n dockermeetup -f stackfile1.yml
 docker-cloud stack update dockermeetup -f stackfile2.yml
+docker-cloud stack update dockermeetup -f stackfile3.yml
+docker-cloud stack update dockermeetup -f stackfile4.yml
+docker-cloud stack update dockermeetup -f blooks-lb.yml
+docker-cloud stack update blooks -f blooks.yml
